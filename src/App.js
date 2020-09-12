114958dba82e2e8348a0baa4a021ef8024ee7de4
Jacky Li <ilikemacaronii@gmail.com>
2020-09-11 17:01:49 -0700

    get basic message functionality working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>Messenger</h1>
-      <input type="text"/>
-      <button>Send Message</button>
+      <input value={input} onChange={event => setInput(event.target.value)}type="text"/>
+      <button onClick={sendMessage}>Send Message</button>
+      {messages.map(message => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
