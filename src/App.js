d01ba0cd19f39c2868f6166e4e93a388fae8fc70
Jacky Li <ilikemacaronii@gmail.com>
2020-09-28 11:46:48 -0700

    add local storage functionality so theres no need to input name on every load

--- src/App.js
+++ src/App.js
@@ -1,53 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { FormControl, Input, IconButton } from '@material-ui/core';
-import './App.css';
-import Message from './Message';
-import { db } from './firebase';
-import firebase from 'firebase';
-import FlipMove from 'react-flip-move';
-import SendIcon from '@material-ui/icons/Send';
+import React, { useEffect, useState } from "react";
+import { FormControl, Input, IconButton } from "@material-ui/core";
+import "./App.css";
+import Message from "./Message";
+import { db } from "./firebase";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
 
 function App() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState(localStorage.getItem("fbmUser1"));
 
   useEffect(() => {
-    db.collection('messages')
-    .orderBy('timestamp', 'desc')
-    .onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})));
-    });
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
   }, []);
   useEffect(() => {
-    setUsername(prompt('Please enter your name'));
+    if (!username) {
+      setUsername(prompt("Please enter your name"));
+    }
   }, []);
   const sendMessage = (event) => {
     event.preventDefault();
-    db.collection('messages').add({
+    db.collection("messages").add({
       username: username,
       message: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-    setInput('');
-  }
+    setInput("");
+    localStorage.setItem("fbmUser1", username);
+  };
 
   return (
     <div className="App">
-      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="Facebook Messenger Logo"/>
-      <h2>Welcome {username}</h2>
+      <img
+        src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
+        alt="Facebook Messenger Logo"
+      />
+      <h2>Welcome {username || "Guest"}</h2>
       <form className="app__form">
-        <FormControl className='app__formControl'>
-          <Input className='app__input' placeholder='Enter a message...' value={input} onChange={event => setInput(event.target.value)}type="text"/>
-          <IconButton className='app__iconButton' disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>
-            <SendIcon/>
+        <FormControl className="app__formControl">
+          <Input
+            className="app__input"
+            placeholder="Enter a message..."
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            type="text"
+          />
+          <IconButton
+            className="app__iconButton"
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            <SendIcon />
           </IconButton>
         </FormControl>
-        
       </form>
       <FlipMove>
-        {messages.map(({id, message}) => (
-          <Message key={id} username={username} message={message}/>
+        {messages.map(({ id, message }) => (
+          <Message key={id} username={username} message={message} />
         ))}
       </FlipMove>
     </div>
